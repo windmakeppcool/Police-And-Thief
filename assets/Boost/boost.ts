378fbcada239923c4096ea1f5f97dcb0ec0e9cb3
lly25782 <lly25782@arcsoft.com.cn>
2026-05-22 14:02:55 +0800

    chore: add .gitattributes to enforce LF line endings

--- assets/Boost/boost.ts
+++ assets/Boost/boost.ts
@@ -1,17 +1,29 @@
-import { _decorator, AssetManager, Canvas, Component, Node } from 'cc';
+import { _decorator, assetManager, AssetManager, Canvas, Component, Node } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('boost')
 export class boost extends Component {
     @property(Canvas) private canvas: Canvas = null;
-    
+
     start() {
 
     }
 
-    private loadBundle(bundleName: string): Promise<AssetManager.Bundle> {
-        
+    private loadBundle(bundleName: string): Promise<AssetManager.Bundle | null> {
+        return new Promise<AssetManager.Bundle | null>(rs => {
+            assetManager.loadBundle(bundleName, (err, bundle) => {
+                if (err) {
+                    console.error(`加载资源包${bundleName}失败: ${err}`);
+                    rs(null);
+                } else {
+                    rs(bundle);
+                }
+            })
+        });
     }
+
+
+
 }
 
 
